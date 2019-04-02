1bf77eb49860504fb77454b9b391a1cca39f90b8
Manoj Choudhari <manoj_choudhari@outlook.com>
2019-04-02 12:59:58 +0200

    Topic sample created

---
+++ TopicSample/Publisher/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "<your_connection_string>";
+        const string TopicName = "<your_topic_name>";
+        static ITopicClient topicClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("PUBSLISHER sending msgs to Topic for next 2 minutes");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync();
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync()
+        {
+            DateTime start = DateTime.Now;
+            try
+            {
+                for (var i = 0; DateTime.Now.Subtract(start).TotalMinutes < 2; i++)
+                {
+                    // Create a new message to send to the topic
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the topic
+                    await topicClient.SendAsync(message);
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
+
